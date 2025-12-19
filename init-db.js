8d4378b1baabe436809e68388a889a3972321186
Alejandro Barrera <alejandro91dark@hotmail.es>
2025-12-18 21:29:50 -0300

    fix: agregar variable de entorno personalizada al crear conexión

--- init-db.js
+++ init-db.js
@@ -15,6 +15,7 @@ async function inicializarBaseDatos() {
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "root",
       password: process.env.DB_PASSWORD || "",
+      port: process.env.DB_PORT || 3306,
       multipleStatements: true, // Permitir múltiples statements
     });
 
